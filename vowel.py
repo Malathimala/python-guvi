5e76144a1a490e8cc17e9b44d235818b1454fb52
Malathimala <46860356+Malathimala@users.noreply.github.com>
2019-01-26 12:37:08 +0530

    Update vowel.py

--- vowel.py
+++ vowel.py
@@ -1,7 +1,7 @@
 ch=input()
 if(ch=='a' or ch=='A' or ch=='e' or ch=='E' or ch=='i' or ch=='I' or ch=='o' or ch=='O' or ch=='u' or ch=='U'):
-    print("vowel")
+    print("Vowel")
 elif((ch>='a' and ch<='z') or (ch>='A' and ch<='Z')):
-    print("consonant")
+    print("Consonant")
 else:
     print("invalid")
