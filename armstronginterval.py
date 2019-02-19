e6632509a2e34d185636ba113b9863d4c31e907c
Malathimala <46860356+Malathimala@users.noreply.github.com>
2019-02-19 13:45:06 +0530

    Create armstronginterval.py

---
+++ armstronginterval.py
@@ -0,0 +1,11 @@
+x,y=map(int,raw_input().split())
+for i in range(x,y):
+    num=i
+    result=0
+    n=len(str(i))
+    while(i!=0):
+        digit=i%10
+        result=result+digit**n
+        i=i//10
+    if num==result:
+        print num,
