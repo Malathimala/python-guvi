8fddb69245c7643c5873c2648bb9d2c95a3d4465
Malathimala <46860356+Malathimala@users.noreply.github.com>
2019-03-07 19:11:14 +0530

    Update holiday.py

--- holiday.py
+++ holiday.py
@@ -1,6 +1,6 @@
 # malathi
-day=str(input())
-if(day=='sunday' or day=='SUNDAY' or day=='saturday' or day=='SATURDAY'):
+b=str(input())
+if(b=='sunday' or b=='saturday' or b=='Saturday' or b=='Sunday' or b=='SUNDAY' or b=='SATURDAY'):
     print("yes")
 else:
     print("no")
