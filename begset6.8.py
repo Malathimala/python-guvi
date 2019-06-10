a7c6c1d5bed509b34f7ffdd479ef3d72ce9aca0c
Malathimala <46860356+Malathimala@users.noreply.github.com>
2019-06-10 16:03:39 +0530

    Create begset6.8.py

---
+++ begset6.8.py
@@ -0,0 +1,16 @@
+# malathi
+a,b=map(int,input().split())
+
+c=[int(b) for b in input().split()]
+
+for i in range(len(c)):
+
+    if(i==b):
+
+        print("yes")
+
+        break
+
+else:
+
+     print("no")
