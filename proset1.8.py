45cc4f8d04ca02caab18d2c55802623b27768fd5
Malathimala <46860356+Malathimala@users.noreply.github.com>
2019-06-15 15:43:59 +0530

    Create proset1.8.py

---
+++ proset1.8.py
@@ -0,0 +1,16 @@
+# malathi
+import math
+
+import functools
+
+a,b=map(int,input().split())
+
+List=[int(i) for i in input().split()]
+
+for i in range(b):
+
+    cc,dd=map(int,input().split())
+
+    t=functools.reduce(math.gcd,List[cc-1:dd])
+
+    print(t)
