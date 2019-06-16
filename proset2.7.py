b5de052472797f166991b16ee6f5d225aa89fa05
Malathimala <46860356+Malathimala@users.noreply.github.com>
2019-06-16 21:24:33 +0530

    Create proset2.7.py

---
+++ proset2.7.py
@@ -0,0 +1,24 @@
+# malathi
+man,san=map(int,input().split())
+
+array=list(map(int,input().split()))
+
+ran=0
+
+for i in range(len(array)):
+
+	for j in range(i+1,len(array)):
+
+		if (array[i]+array[j]==san):
+
+			ran+=1
+
+			break
+
+if(ran):
+
+	print("yes")
+
+else:
+
+	print("no")
