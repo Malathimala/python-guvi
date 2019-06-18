8656eb02a68c455de4f800293766cb11269b7445
Malathimala <46860356+Malathimala@users.noreply.github.com>
2019-06-18 20:26:04 +0530

    Create proset3.3.py

---
+++ proset3.3.py
@@ -0,0 +1,10 @@
+# malathi
+string=input()
+
+if string.find("GLGLGL")!=-1 or string.find("GRGRGR")!=-1 or string.find("GLLG")!=-1 or string.find("GRRG")!=-1:
+
+    print("yes")
+
+else:
+
+    print("no")
